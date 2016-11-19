7e8a23e791f3a8303120cdd9510d45e531d9ae7e
mdignan <maxdignan@gmail.com>
2016-11-19 18:23:58 -0500

    dn

--- app/parallel/db/schema.rb
+++ app/parallel/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119201103) do
+ActiveRecord::Schema.define(version: 20161119230356) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20161119201103) do
     t.float    "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "author"
   end
 
   create_table "books_users", force: :cascade do |t|
--- app/parallel/db/seeds.rb
+++ app/parallel/db/seeds.rb
@@ -14,16 +14,16 @@ User.create([
 ])
 
 Book.create([
-  {title: 'Automation and Robotics', img: 'automation-and-robotics.jpg', pdf: 'automation-and-robotics.pdf', price: 20.0},
-  {title: 'Basics of Accounting-information-processing', img: 'basics-of-accounting-information-processing.jpg', pdf: 'basics-of-accounting-information-processing.pdf', price: 20.0},
-  {title: 'Cost Analysis', img: 'cost-analysis.jpg', pdf: 'cost-analysis.pdf', price: 19.50},
-  {title: 'English', img: 'english', pdf: 'english.pdf', price: 19.50},
-  {title: 'Essential Study Skills', img: 'essential-study-skills.jpg', pdf: 'essential-study-skills.pdf', price: 19.50},
-  {title: 'How to Write an Essay', img: 'how-to-write-an-essay.jpg', pdf: 'how-to-write-an-essay.pdf', price: 19.50},
-  {title: 'HRM Managing the Human Ressource', img: 'hrm-managing-the-human-ressource.jpg', pdf: 'hrm-managing-the-human-ressource.pdf', price: 19.50},
-  {title: 'Innovative Service Design Workbook', img: 'innovative-service-design-workbook.jpg', pdf: 'innovative-service-design-workbook.pdf', price: 19.50},
-  {title: 'Modern Microeconomics', img: 'modern-microeconomics.jpg', pdf: 'modern-microeconomics.pdf', price: 19.50},
-  {title: 'Technology Based-Entrepreneurship', img: 'technology-based-entrepreneurship.jpg', pdf: 'technology-based-entrepreneurship.pdf', price: 19.50},
-  {title: 'Working Capital Strategic Management Exercises', img: 'working-capital-strategic-management-exercises.jpg', pdf: 'working-capital-strategic-management-exercises.pdf', price: 19.50},
+  {title: 'Automation and Robotics', author: 'Alan C.', img: 'automation-and-robotics.jpg', pdf: 'automation-and-robotics.pdf', price: 20.0},
+  {title: 'Basics of Accounting-information-processing', author: 'A. Dowl', img: 'basics-of-accounting-information-processing.jpg', pdf: 'basics-of-accounting-information-processing.pdf', price: 20.0},
+  {title: 'Cost Analysis', author: 'L. Jain', img: 'cost-analysis.jpg', pdf: 'cost-analysis.pdf', price: 19.50},
+  {title: 'English', author: 'J.V. Rossenburg', img: 'english', pdf: 'english.pdf', price: 19.50},
+  {title: 'Essential Study Skills', author: 'William Derek', img: 'essential-study-skills.jpg', pdf: 'essential-study-skills.pdf', price: 19.50},
+  {title: 'How to Write an Essay', author: 'Cody Winheiser', img: 'how-to-write-an-essay.jpg', pdf: 'how-to-write-an-essay.pdf', price: 19.50},
+  {title: 'HRM Managing the Human Ressource', author: 'Levi Von Strauss', img: 'hrm-managing-the-human-ressource.jpg', pdf: 'hrm-managing-the-human-ressource.pdf', price: 19.50},
+  {title: 'Innovative Service Design Workbook', author: 'Mary Poppins II', img: 'innovative-service-design-workbook.jpg', pdf: 'innovative-service-design-workbook.pdf', price: 19.50},
+  {title: 'Modern Microeconomics', author: 'Jamey Diamon', img: 'modern-microeconomics.jpg', pdf: 'modern-microeconomics.pdf', price: 19.50},
+  {title: 'Technology Based-Entrepreneurship', author: 'Murk Zackerburg', img: 'technology-based-entrepreneurship.jpg', pdf: 'technology-based-entrepreneurship.pdf', price: 19.50},
+  {title: 'Working Capital Strategic Management Exercises', author: 'M.D. Richfield', img: 'working-capital-strategic-management-exercises.jpg', pdf: 'working-capital-strategic-management-exercises.pdf', price: 19.50},
 
 ])
